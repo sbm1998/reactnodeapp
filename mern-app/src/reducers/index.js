89c8462be800b79fd0f1ac6418bb5f201fe5fa3e
Shubham Gangrade <34281068+sbm1998@users.noreply.github.com>
2021-08-09 02:02:47 +0530

    Delete mern-app directory

--- mern-app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import usersData from './usersData'
-import authenticationReducer from './authenticationReducer';
-
-export default combineReducers({
-    usersData,
-    authenticationReducer
-});
