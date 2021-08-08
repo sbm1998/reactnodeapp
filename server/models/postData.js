06276a49927405a6cd2f9e19942d32feae458539
Shubham Gangrade <34281068+sbm1998@users.noreply.github.com>
2021-08-09 02:06:37 +0530

    Add files via upload

---
+++ server/models/postData.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const dataSchema=mongoose.Schema({
+    name: String,
+    email: String,
+    city: String,
+    password:String,
+})
+
+var datainfo = mongoose.model('datainfo',dataSchema);
+//
+export default datainfo;
---
+++ server/models/usersData.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userData = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+    },
+});
+var userDetail = mongoose.model("userDetail",userData);
+
+export default userDetail;
